4511d1a45546acd85d6fe1b3df010ffa70f3b4c9
tangmingchao <18798805737@139.com>
2016-12-10 12:17:05 +0800

    edit config/routes.rb#aponitment and interface_document

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :appointments
-  resources :interface_documents
+  resources :appointments, only: [:index, :show, :create]
+  resources :interface_documents, only: [:index, :show, :create, :update, :destroy]
   devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
