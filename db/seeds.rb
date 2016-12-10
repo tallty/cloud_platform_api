e21a1b9da4a84c87c3f3d7f5264070c03f259a5a
tangmingchao <18798805737@139.com>
2016-12-10 09:43:33 +0800

    add test file db/seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Admin.destroy_all
+User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Admin.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
