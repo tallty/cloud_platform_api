b778f5d86dcd7563ce846cbedb9504878dc721f8
tangmingchao <18798805737@139.com>
2016-12-10 17:53:52 +0800

    edit config/routes.rb#sms_token

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,12 @@ Rails.application.routes.draw do
   	resources :appointments, only: [:index, :show]
     resources :interface_documents, only: [:index, :show, :create, :update, :destroy]
   end
+
+  ############ SMS Routes ###################
+  resources :sms_tokens, only: [:show]  do
+    collection do
+      post 'register'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
