bc3e774045fe98885a8f22e9298348a8b3eb7279
tangmingchao <18798805737@139.com>
2016-12-10 12:23:44 +0800

    edit config/routes.rb#admin/

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :appointments, only: [:index, :show, :create]
-  resources :interface_documents, only: [:index, :show, :create, :update, :destroy]
+  resources :interface_documents, only: [:index, :show]
+
+  ################### devise_for ####################
   devise_for :admins
   devise_for :users
+
+  ################### admin #########################
+  namespace :admin do
+  	resources :appointments, only: [:index, :show]
+    resources :interface_documents, only: [:index, :show, :create, :update, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
