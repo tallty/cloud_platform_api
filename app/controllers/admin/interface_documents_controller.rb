cb3e86c5fc6accb981bcabc365cad5c079514ecf
tangmingchao <18798805737@139.com>
2016-12-10 12:39:38 +0800

    edit interface_documents_controller.rb

--- app/controllers/admin/interface_documents_controller.rb
+++ app/controllers/admin/interface_documents_controller.rb
@@ -1,10 +1,13 @@
 class Admin::InterfaceDocumentsController < ApplicationController
+  acts_as_token_authentication_handler_for Admin
   before_action :set_admin_interface_document, only: [:show, :update, :destroy]
 
   respond_to :json
 
   def index
-    @admin_interface_documents = Admin::InterfaceDocument.all
+    page = params[:page] || 1
+    per_page = params[:per_page] || 20
+    @admin_interface_documents = InterfaceDocument.all.paginate(page: page, per_page: per_page)
     respond_with(@admin_interface_documents)
   end
 
@@ -13,14 +16,18 @@ class Admin::InterfaceDocumentsController < ApplicationController
   end
 
   def create
-    @admin_interface_document = Admin::InterfaceDocument.new(admin_interface_document_params)
-    @admin_interface_document.save
-    respond_with(@admin_interface_document)
+    @admin_interface_document = InterfaceDocument.new(admin_interface_document_params)
+    if @admin_interface_document.save
+      respond_with(@admin_interface_document, template:"admin_interface_documents/show", status: 201)  
+    else
+      @error = "接口文档创建 失败 ！"
+      respond_with(@error)
+    end  
   end
 
   def update
     @admin_interface_document.update(admin_interface_document_params)
-    respond_with(@admin_interface_document)
+    respond_with(@admin_interface_document, template:"admin_interface_documents/show", status: 201)
   end
 
   def destroy
@@ -30,10 +37,10 @@ class Admin::InterfaceDocumentsController < ApplicationController
 
   private
     def set_admin_interface_document
-      @admin_interface_document = Admin::InterfaceDocument.find(params[:id])
+      @admin_interface_document = InterfaceDocument.find(params[:id])
     end
 
     def admin_interface_document_params
-      params[:admin_interface_document]
+      params.require(:interface_document).permit(:title, :description, :site)
     end
 end
--- app/controllers/interface_documents_controller.rb
+++ app/controllers/interface_documents_controller.rb
@@ -1,10 +1,12 @@
 class InterfaceDocumentsController < ApplicationController
-  before_action :set_interface_document, only: [:show, :edit, :update, :destroy]
+  before_action :set_interface_document, only: [:show]
 
-  respond_to :html
+  respond_to :json
 
   def index
-    @interface_documents = InterfaceDocument.all
+    page = params[:page] || 1
+    per_page = params[:per_page] || 20
+    @interface_documents = InterfaceDocument.all.paginate(page: page, per_page: per_page)
     respond_with(@interface_documents)
   end
 
@@ -12,36 +14,8 @@ class InterfaceDocumentsController < ApplicationController
     respond_with(@interface_document)
   end
 
-  def new
-    @interface_document = InterfaceDocument.new
-    respond_with(@interface_document)
-  end
-
-  def edit
-  end
-
-  def create
-    @interface_document = InterfaceDocument.new(interface_document_params)
-    @interface_document.save
-    respond_with(@interface_document)
-  end
-
-  def update
-    @interface_document.update(interface_document_params)
-    respond_with(@interface_document)
-  end
-
-  def destroy
-    @interface_document.destroy
-    respond_with(@interface_document)
-  end
-
   private
     def set_interface_document
       @interface_document = InterfaceDocument.find(params[:id])
-    end
-
-    def interface_document_params
-      params.require(:interface_document).permit(:title, :description, :site)
     end
 end
