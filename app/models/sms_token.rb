a4f519f2a75161ce5a787e8ed98da6396ed1adb5
tangmingchao <18798805737@139.com>
2016-12-10 17:49:42 +0800

    edit app/models/sms_token.rb

--- app/models/sms_token.rb
+++ app/models/sms_token.rb
@@ -10,4 +10,19 @@
 #
 
 class SmsToken < ApplicationRecord
+
+  def self.register phone
+  	token = (0..9).to_a.sample(4).jion
+  	sms_token = SmsToken.find_or_initialize_by(phone: phone)
+
+  	if phone.present?
+  	  tpl_id = 1588030
+  	  sms_hash = {code: token}
+  	  ChinaSMS.use :yunpian, password: "255281473668c1ef1fc752b71ce575d8"
+      result = ChinaSMS.to phone, sms_hash, {tpl_id: tpl_id}
+      sms_token.token = token
+      sms_token.save 
+  	end
+  	sms_token
+  end
 end
