17b3db9125617e15bf6e654a185b1d9e2047f3a8
tangmingchao <18798805737@139.com>
2016-12-12 10:06:34 +0800

    edit spec/spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,8 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'factory_girl_rails'
+
 RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
