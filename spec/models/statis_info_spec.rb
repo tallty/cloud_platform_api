f8831c7c8d6ce753246b5d7594d7cd96d8aef8bc
tangmingchao <18798805737@139.com>
2016-12-13 17:12:44 +0800

    edit spec/models/statis_info_spec.rb

--- spec/models/statis_info_spec.rb
+++ spec/models/statis_info_spec.rb
@@ -12,5 +12,6 @@
 require 'rails_helper'
 
 RSpec.describe StatisInfo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:interface_document) }
 end
