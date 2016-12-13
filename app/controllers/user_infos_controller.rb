7b76a456e0440e8751d5389dcfd13ac061766075
tangmingchao <18798805737@139.com>
2016-12-13 11:10:28 +0800

    edit app/controllers/user_infos_controller.rb

--- app/controllers/user_infos_controller.rb
+++ app/controllers/user_infos_controller.rb
@@ -1,22 +1,11 @@
 class UserInfosController < ApplicationController
   acts_as_token_authentication_handler_for User, except: [:reset] 
 
-  before_action :set_user_info, only: [:show, :update, :destroy]
+  before_action :set_user_info, only: [:show, :update]
 
   respond_to :json
 
-  def index
-    @user_infos = UserInfo.all
-    respond_with(@user_infos)
-  end
-
   def show
-    respond_with(@user_info)
-  end
-
-  def create
-    @user_info = UserInfo.new(user_info_params)
-    @user_info.save
     respond_with(@user_info)
   end
 
@@ -24,12 +13,7 @@ class UserInfosController < ApplicationController
     @user_info.update(user_info_params)
     respond_with(@user_info, template: "user_infos/show")
   end
-
-  def destroy
-    @user_info.destroy
-    respond_with(@user_info)
-  end
-
+  
   def reset
     @user = User.reset_user_password reset_params 
     respond_with @user
