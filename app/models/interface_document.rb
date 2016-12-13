76b6f0fc7a238fe91917305554703b715e6c6077
tangmingchao <18798805737@139.com>
2016-12-13 17:23:14 +0800

    edit app/models/interface_document.rb

--- app/models/interface_document.rb
+++ app/models/interface_document.rb
@@ -16,6 +16,15 @@ class InterfaceDocument < ApplicationRecord
   has_many :users, through: :appointments
   has_many :statis_infos, dependent: :destroy
 
+  ############# 创建统计信息 #################
+  def ceate_statis_info (user_id, interface_document_id)
+  	_statis_info = self.statis_infos.create!(
+  	  user_id: user_id, 
+  	  interface_document_id:interface_document_id
+  	)
+  	_statis_info.save!
+  end
+
   ############### 排名情况 ###################
   def total_rank#总的排名
   	self.class.where("frequency > :frequency", frequency: frequency).count + 1
