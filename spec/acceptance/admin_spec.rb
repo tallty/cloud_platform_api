db090053e2bac74a48ccc792b7ae096851786ef0
tangmingchao <18798805737@139.com>
2016-12-14 14:58:17 +0800

    edit spec/acceptance/admin_spec.rb# sign_up

--- spec/acceptance/admin_spec.rb
+++ spec/acceptance/admin_spec.rb
@@ -3,32 +3,54 @@ require 'acceptance_helper'
 resource "管理后台相关接口" do
   header "Accept", "application/json"
 
-  describe 'admin authentication' do
+  post "/admins" do
+    admin_attrs = FactoryGirl.attributes_for :admin
+    
+    parameter :email, "管理员注册的邮箱号", required: true, scope: :admin
+    parameter :password, "管理员注册的密码", required: true, scope: :admin
+
+    describe "管理员注册状态" do
+      let(:email) { admin_attrs[:email] }
+      let(:password) { admin_attrs[:password] }
+
+      response_field :id, "管理员ID"
+      response_field :email, "邮箱"
+      response_field :authentication_token, "鉴权Token"
+      response_field :created_at, "创建时间"
+      response_field :updated_at, "更新时间"
+      
+      example "管理员注册成功" do
+        do_request
+        puts response_body
+        expect(status).to eq(201)
+      end
+    end
+  end
+
+  ################ sign_in ##############
+  post "/admins/sign_in" do
+    admin_attrs = FactoryGirl.attributes_for :admin
 
     before do
       @admin = create(:admin)
     end
 
-    post "/admins/sign_in" do
+    parameter :email, "登录的邮箱", required: true, scope: :admin
+    parameter :password, "登录密码", required: true, scope: :admin
 
-      parameter :email, "登录的邮箱", required: true, scope: :admin
-      parameter :password, "登录密码", required: true, scope: :admin
+    let(:email) { admin_attrs[:email] }
+    let(:password) { admin_attrs[:password] }
 
-      admin_attrs = FactoryGirl.attributes_for :admin
-      let(:email) { admin_attrs[:email] }
-      let(:password) { admin_attrs[:password] }
+    response_field :id, "管理员ID"
+    response_field :email, "邮箱"
+    response_field :created_at, "创建时间"
+    response_field :updated_at, "更新时间"
+    response_field :authentication_token, "鉴权Token"
 
-      response_field :id, "用户ID"
-      response_field :email, "邮箱"
-      response_field :created_at, "创建时间"
-      response_field :updated_at, "更新时间"
-      response_field :authentication_token, "鉴权Token"
-
-      example "管理员登录成功" do
-        do_request
-        puts response_body
-        expect(status).to eq(201)
-      end
+    example "管理员登录成功" do
+      do_request
+      puts response_body
+      expect(status).to eq(201)
     end
   end
 end
