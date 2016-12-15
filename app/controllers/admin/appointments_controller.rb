0f4d9a8dbf631c7aa8261f1458d1b3ddfff34069
tangmingchao <18798805737@139.com>
2016-12-15 12:11:45 +0800

    edit app/controllers/admin/appointments_controller.rb#template

--- app/controllers/admin/appointments_controller.rb
+++ app/controllers/admin/appointments_controller.rb
@@ -17,12 +17,12 @@ class Admin::AppointmentsController < ApplicationController
 
   def accept
     @admin_appointment.accept!
-    respond_with(@admin_appointment, template:"admin/admin_appointments/show", status: 201)
+    respond_with(@admin_appointment, template:"admin/appointments/show", status: 201)
   end
 
   def refuse
     @admin_appointment.refuse!
-    respond_with(@admin_appointment, template:"admin/admin_appointments/show", status: 201)
+    respond_with(@admin_appointment, template:"admin/appointments/show", status: 201)
   end
 
   private
