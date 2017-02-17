758e6c1df6aae1e9f89cca662e54b56b63b42862
mumumumushu <mumumumushu@gmail.com>
2017-02-17 11:16:25 +0800

    update spec for the last cm

--- spec/acceptance/admin_spec.rb
+++ spec/acceptance/admin_spec.rb
@@ -54,59 +54,64 @@ resource "管理员 帐号管理 相关接口" do
     end
   end
 
-   
-  #################### index #########################
-  get '/admin/manager_accounts' do
-    before do
-      @admins = create_list(:admin, 1)
+   describe "管理员端 管理员账号相关接口" do 
+    admin_attrs = FactoryGirl.attributes_for(:admin)
+
+    header "X-Admin-Token", admin_attrs[:authentication_token]
+    header "X-Admin-Email", admin_attrs[:email]
+    #################### index #########################
+    get '/admin/manager_accounts' do
+      before do
+        @admins = create_list(:admin, 1)
+      end
+
+      parameter :page, "当前页", required: false
+      parameter :per_page, "每页的数量", required: false
+
+      let(:page) {1}
+      let(:per_page) {15}
+
+      example "管理员获取 管理员账户列表成功" do
+        do_request
+        puts response_body
+        expect(status).to eq(200)
+      end
     end
 
-    parameter :page, "当前页", required: false
-    parameter :per_page, "每页的数量", required: false
+    ##################### show ########################
+    get '/admin/manager_accounts/:id' do
+      before do
+        @admins = create_list(:admin, 1)
+      end
+      
+      let(:id) { @admins.first.id }
 
-    let(:page) {1}
-    let(:per_page) {15}
-
-    example "管理员获取 管理员账户列表成功" do
-      do_request
-      puts response_body
-      expect(status).to eq(200)
+      example "管理员 查看指定管理员账户 详情成功" do
+        do_request
+        puts response_body
+        expect(status).to eq(200)
+      end
     end
-  end
 
-  ##################### show ########################
-  get '/admin/manager_accounts/:id' do
-    before do
-      @admins = create_list(:admin, 1)
-    end
-    
-    let(:id) { @admins.first.id }
+    ##################### delete ########################
+    delete '/admin/manager_accounts/:id' do
+      # admin_attrs = FactoryGirl.attributes_for(:admin)
 
-    example "管理员 查看指定管理员账户 详情成功" do
-      do_request
-      puts response_body
-      expect(status).to eq(200)
-    end
-  end
+      # header "X-Admin-Token", admin_attrs[:authentication_token]
+      # header "X-Admin-Phone", admin_attrs[:email]
 
-  ##################### delete ########################
-  delete '/admin/manager_accounts/:id' do
-    # admin_attrs = FactoryGirl.attributes_for(:admin)
+      before do
+        # @admin = create(:admin)
+        @admins = create_list(:admin, 1)
+      end
+      
+      let(:id) { @admins.first.id }
 
-    # header "X-Admin-Token", admin_attrs[:authentication_token]
-    # header "X-Admin-Phone", admin_attrs[:email]
-
-    before do
-      # @admin = create(:admin)
-      @admins = create_list(:admin, 1)
-    end
-    
-    let(:id) { @admins.first.id }
-
-    example "管理员 删除指定管理员账户 详情成功" do
-      do_request
-      puts response_body
-      expect(status).to eq(204)
+      example "管理员 删除指定管理员账户 详情成功" do
+        do_request
+        puts response_body
+        expect(status).to eq(204)
+      end
     end
   end
 end
