a0c26b334f50fcd1ec19f9f86d2f62637c9b5939
mumumumushu <mumumumushu@gmail.com>
2017-02-23 08:55:33 +0800

    remove the part of statistic about interface_document

--- app/models/interface_document.rb
+++ app/models/interface_document.rb
@@ -58,92 +58,92 @@ class InterfaceDocument < ApplicationRecord
     _output
   end
   ############################################ 排名情况 ######################################
-  def total_rank#总的排名
-  	self.class.where("frequency > ?", self.frequency).count + 1
-  end
+  # def total_rank#总的排名
+  # 	self.class.where("frequency > ?", self.frequency).count + 1
+  # end
 
-  def year_rank#年排名
-    count = 1
-    InterfaceDocument.all.each do |api|
-      count = count + 1 if api.year_frequency > self.year_frequency
-    end
-    count 
-  end
+  # def year_rank#年排名
+  #   count = 1
+  #   InterfaceDocument.all.each do |api|
+  #     count = count + 1 if api.year_frequency > self.year_frequency
+  #   end
+  #   count 
+  # end
 
-  def month_rank#月排名
-  	count = 1
-    InterfaceDocument.all.each do |api|
-      count = count + 1 if api.month_frequency > self.month_frequency
-    end
-    count 
-  end
+  # def month_rank#月排名
+  # 	count = 1
+  #   InterfaceDocument.all.each do |api|
+  #     count = count + 1 if api.month_frequency > self.month_frequency
+  #   end
+  #   count 
+  # end
 
-  def week_rank#周排名
-  	count = 1
-    InterfaceDocument.all.each do |api|
-      count = count + 1 if api.week_frequency > self.week_frequency
-    end
-    count 
-  end
+  # def week_rank#周排名
+  # 	count = 1
+  #   InterfaceDocument.all.each do |api|
+  #     count = count + 1 if api.week_frequency > self.week_frequency
+  #   end
+  #   count 
+  # end
 
-  def day_rank#日排名
-  	count = 1
-    InterfaceDocument.all.each do |api|
-      count = count + 1 if api.day_frequency > self.day_frequency
-    end
-    count 
-  end
+  # def day_rank#日排名
+  # 	count = 1
+  #   InterfaceDocument.all.each do |api|
+  #     count = count + 1 if api.day_frequency > self.day_frequency
+  #   end
+  #   count 
+  # end
 
-  def hour_rank#小时排名
-  	count = 1
-    InterfaceDocument.all.each do |api|
-      count = count + 1 if api.hour_frequency > self.hour_frequency
-    end
-    count 
-  end
+  # def hour_rank#小时排名
+  # 	count = 1
+  #   InterfaceDocument.all.each do |api|
+  #     count = count + 1 if api.hour_frequency > self.hour_frequency
+  #   end
+  #   count 
+  # end
 
-  ############################################ 访问量情况 ########################################
-  def total_frequency#总访问量
-  	self.frequency
-  end
+  # ############################################ 访问量情况 ########################################
+  # def total_frequency#总访问量
+  # 	self.frequency
+  # end
 
-  def hour_frequency#时访问量
-    if self.statis_infos.present?
-       self.statis_infos.where("created_at > ?", Time.zone.now - 1.hour).count
-    else
-      0
-    end
-  end
+  # def hour_frequency#时访问量
+  #   if self.statis_infos.present?
+  #      self.statis_infos.where("created_at > ?", Time.zone.now - 1.hour).count
+  #   else
+  #     0
+  #   end
+  # end
 
-  def day_frequency#天访问量
-    if self.statis_infos.present?
-      self.statis_infos.where("created_at > ?", Time.zone.now.midnight).count #从当日0点0份分0秒到现在
-    else
-      0
-    end
-  end
+  # def day_frequency#天访问量
+  #   if self.statis_infos.present?
+  #     self.statis_infos.where("created_at > ?", Time.zone.now.midnight).count #从当日0点0份分0秒到现在
+  #   else
+  #     0
+  #   end
+  # end
 
-  def week_frequency#周访问量 #####
-    if self.statis_infos.present?
-      self.statis_infos.where("created_at > ?", Time.zone.now - Time.zone.now.wday.days).count #从本周的周一到现在
-    else
-      0
-    end
-  end
+  # def week_frequency#周访问量 #####
+  #   if self.statis_infos.present?
+  #     self.statis_infos.where("created_at > ?", Time.zone.now - Time.zone.now.wday.days).count #从本周的周一到现在
+  #   else
+  #     0
+  #   end
+  # end
 
-  def month_frequency#月访问量55
-    if self.statis_infos.present?
-      self.statis_infos.where("created_at > ?", Time.zone.now - Time.zone.now.day.days).count #从本月的1号到现在
-    else
-      0
-    end
-  end
+  # def month_frequency#月访问量55
+  #   if self.statis_infos.present?
+  #     self.statis_infos.where("created_at > ?", Time.zone.now - Time.zone.now.day.days).count #从本月的1号到现在
+  #   else
+  #     0
+  #   end
+  # end
 
-  def year_frequency#年访问量
-    if self.statis_infos.present?
-      self.statis_infos.where("created_at > ?",  Time.zone.now - Time.zone.now.yday.days).count  #从本年的1月1号到现在  
-    else
-      0
-    end
-  end
+  # def year_frequency#年访问量
+  #   if self.statis_infos.present?
+  #     self.statis_infos.where("created_at > ?",  Time.zone.now - Time.zone.now.yday.days).count  #从本年的1月1号到现在  
+  #   else
+  #     0
+  #   end
+  # end
 end
--- app/views/interface_documents/_interface_document.json.jbuilder
+++ app/views/interface_documents/_interface_document.json.jbuilder
@@ -1,4 +1,2 @@
-json.extract! interface_document, :id, :title, :description, :site, :total_frequency, :total_rank, :created_at, :updated_at,
- :hour_frequency, :day_frequency, :week_frequency, :month_frequency, :year_frequency, 
- :year_rank, :month_rank, :week_rank, :day_rank, :hour_rank
+json.extract! interface_document, :id, :title, :description, :site, :created_at, :updated_at
 json.url interface_document_url(interface_document, format: :json)
